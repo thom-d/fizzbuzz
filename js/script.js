761d36b6c0b5f2bb473eea6e7fa16b98ba268a1e
thom-d <d.thomas014@gmail.com>
2015-10-10 21:43:33 -0400

    js correction

--- js/script.js
+++ js/script.js
@@ -1,6 +1,18 @@
-confirm ("I am so ready to play!");
+//for the variable i, it equals 1 and if 1 is less than or equal to 100 add 1.
 
-if (score >= 90) {
-        console.log('You got an A.');
-} else if (score >= 80) {
-        console.log('You got a B.');
+//if i is divisible by 3 or 5 print FizzBuzz
+
+for (var i = 1; i <= 100; i++) {
+  if(i % 3 === 0 && i % 5 === 0) {
+    console.log('FizzBuzz');
+  }
+  else if(i % 3 === 0) {
+    console.log('fizz');
+  } 
+  else if(i % 5 === 0){
+    console.log('buzz');
+  }
+  else {
+    console.log(i);
+  }
+}
